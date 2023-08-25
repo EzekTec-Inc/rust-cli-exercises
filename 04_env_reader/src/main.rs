843543c1baca9ac3ae23246ea0578e4551fccc37
EzekTec <stephen.ezekwem@ezektec.com>
2023-08-25 14:44:08 -0600

    Updated the main file to read .env files

--- 04_env_reader/src/main.rs
+++ 04_env_reader/src/main.rs
@@ -1,3 +1,14 @@
+use dotenv_codegen::dotenv;
+
 fn main() {
-    todo!("replace this line with your code!")
+    dotenv::dotenv().ok();
+    println!(
+        "value for the env var MY_VAR_1 is: {}",
+        String::from(dotenv!("MY_VAR_1"))
+    );
+
+    println!(
+        "value for the env var MY_VAR_2 is: {}",
+        String::from(dotenv!("MY_VAR_2"))
+    )
 }
